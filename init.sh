64b22d2b60a8a9ffd9114729e49f68528f80b9bd
Tal Muskal <talm@adextent.com>
2017-11-17 05:49:25 -0800

    progress

--- init.sh
+++ init.sh
@@ -12,7 +12,11 @@ DEFAULT_DOMAIN=${DOMAIN:-example.com}
 DOMAIN=`dlg --title 'Domain' --inputbox 'Enter the domain' 0 0 $DEFAULT_DOMAIN`
 # TODO: generate user and role for cluster
 # TODO: add cost allocation tags
-
+# TODO: todo ldap compatible dir
+# TODO: https://funktion.fabric8.io/docs/#installing-runtimes-and-connectors
+# TODO: ipsilon
+# TODO: freeipa and samba
+# TODO: hook LDAP to all services.
 PREFIX=`dlg --title 'Name' --inputbox 'Enter cluster name' 0 0 k8a`
 export NAME=$PREFIX.$DOMAIN
 export KOPS_STATE_STORE=s3://k8.$DOMAIN
@@ -27,6 +31,8 @@ ZONES=$(printf ",%s" "${ZONES_TMP[@]}")
 ZONES=${ZONES:1}
 kops="docker run --rm -it -e AWS_ACCESS_KEY_ID -e AWS_SECRET_ACCESS_KEY -e AWS_DEFAULT_REGION=$THE_REGION -e KOPS_STATE_STORE -v `pwd`:/tmp2 -v `pwd`/out/:/out/ -v $HOME/.kube/:/root/.kube/ pottava/kops:1.5"
 kops="kops"
+helm="docker run --rm -it -v $HOME/.kube/:/root/.kube/ linkyard/docker-helm"
+# helm="helm"
 kubectl="docker run --rm -it -v $HOME/.kube/:/root/.kube/ -v $PWD/manifests/:/manifests/ pottava/kubectl"
 ktmpl="docker run --rm -v $PWD:/project -t inquicker/ktmpl"
 # Setup configuration
@@ -44,8 +50,12 @@ dashboard "Deploy Dashboard" on \
 heapster "Deploy Heapster" on \
 ssl "Generate Certificate for *.$Domain" on \
 nodesautoscaling "Node Autoscaling" on \
+helm "Init helm" on \
+fabric8 "fabric8" on \
+che "Che" on \
 secrets "Config Secrets" on`
 # upload configuration to S3
+
 function enabled(){
 	case "${SELECTED_COMPONENTS=[@]}" in  *"$1"*) return 0;; esac
 	return 1;
@@ -69,7 +79,6 @@ then
 	fi	
 fi
 # Create Cluster
-
 if enabled "clusters"
 then
 	set +e
@@ -94,8 +103,7 @@ then
 	if [ $CLUSTER_NOT_EXIST -eq 0 ]
 	then
 		$kops update cluster ${NAME} --yes
-		echo Launched. sleeping
-		$kops validate cluster
+		echo Launched. sleeping		
 	else 
 		echo "skipping cluster launch"		
 	fi
@@ -115,6 +123,31 @@ then
 		sleep 5
 	done	
 	echo 100 | dialog --gauge "Cluster ready" 10 70 0
+fi
+
+if enabled "helm"
+then
+	helm init
+fi
+
+if enabled "che"
+then
+	$kubectl apply -f http://central.maven.org/maven2/io/fabric8/online/apps/che/1.0.54/che-1.0.54-kubernetes.yml
+	$kubectl apply -f http://central.maven.org/maven2/io/fabric8/devops/apps/orion/2.2.327/orion-2.2.327-kubernetes.yml 
+fi
+ 
+if enabled "fabric8"
+then
+	set +e
+	$kubectl delete namespace fabric8
+	set -e
+	curl -sS https://get.fabric8.io/download.txt | bash	
+	dlg --clear --insecure --passwordbox "Enter your github OAUTH Client ID" 10 50 > tmp
+	GITHUB_OAUTH_CLIENT_ID=`cat tmp`
+	dlg --clear --insecure --passwordbox "Enter your gitlab OAUTH Client Secret" 10 50 > tmp
+	GITHUB_OAUTH_CLIENT_SECRET=`cat tmp`
+	rm tmp
+	~/.fabric8/bin/gofabric8 deploy --package system --http=true --legacy=false -n fabric8 -y
 fi
 
 if enabled "secrets"
@@ -169,12 +202,32 @@ fi
 if enabled "dockerregistry"
 then
 	echo Deploying docker registry
+	$kubectl apply -f http://central.maven.org/maven2/io/fabric8/devops/apps/nexus3/2.2.327/nexus3-2.2.327-kubernetes.yml 
+fi
+if enabled "nexus"
+then
+	echo deploying nexus3
+	$kubectl apply -f http://central.maven.org/maven2/io/fabric8/devops/apps/nexus3/2.2.327/nexus3-2.2.327-kubernetes.yml 
+fi
+if enabled "letschat"
+then
+	echo deploying letschat
+	$kubectl apply -f http://central.maven.org/maven2/io/fabric8/devops/apps/letschat/2.2.327/letschat-2.2.327-kubernetes.yml
 fi
 
 if enabled "gitlab"
 then
-	echo Deploying gitlab
+	echo Deploying gitlab	
+	set +e
+	$helm del --purge gitlab
+	set -e
+	dlg --clear --insecure --passwordbox "Enter your gitlab root password" 10 50 > gitlab.passwd.tmp
+	PASSWD=`cat gitlab.passwd.tmp`
+	rm gitlab.passwd.tmp	
+	$helm install --name gitlab --set gitlabRootPassword="$PASSWD",externalUrl=http://gitlab.cybercyder.com/ stable/gitlab-ce
+	$kubectl get svc --namespace default gitlab-gitlab-ce -o jsonpath='{.status.loadBalancer.ingress[0].hostname}'
 	# claim efs volume
+	# set cname
 	# apply gitlab	
 fi
 
